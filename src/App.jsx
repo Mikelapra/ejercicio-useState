ac5029fd09b508f2e13387e9649d308be3561c73
unknown <apraiz.mikel@gmail.com>
2024-03-13 21:48:17 +0100

    Spring 19 Entregable 1 terminado sin Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
- //Aquí tu código
+  // Destructura usaState en sus dos elementos y se inicie siempre con "Sofía"
+  const [name, setName] = useState('Sofía');
+
+
+  // Función para cambiar el nombre del profesor
+  const newTeacherName = (e) => {
+    setName(e);
+  };
+
+  return (
+    <div>
+      <h2>Teacher Name {name}</h2>
+      <ul>
+        <li onClick={() => newTeacherName('Data')}>Data</li>
+        <li onClick={() => newTeacherName('Reyes')}>Reyes</li>
+        <li onClick={() => newTeacherName('Yolanda')}>Yolanda</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
